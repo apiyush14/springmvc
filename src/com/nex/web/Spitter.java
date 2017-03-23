419139aa1a8c597960bcf953e406e1a1ee5d1ed7
piyush <piyush@localhost.localdomain>
2017-03-23 23:34:40 +0530

    Registration

--- src/com/nex/web/Spitter.java
+++ src/com/nex/web/Spitter.java
@@ -5,6 +5,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class Spitter {
  public String firstName;
+ public String lastName;
+ public String email;
+ public String username;
+ public String password;
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
 
 public String getFirstName() {
 	return firstName;
