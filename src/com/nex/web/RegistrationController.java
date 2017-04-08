5940539da917349adab6d846b1ad3a1f3461fb02
piyush <piyush@localhost.localdomain>
2017-04-08 20:29:55 +0530

    Registration

--- src/com/nex/web/RegistrationController.java
+++ src/com/nex/web/RegistrationController.java
@@ -2,6 +2,9 @@ package com.nex.web;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.BooleanOperators.Or;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RegistrationController 
 {
+	@Autowired
+	MongoOperations mongo;
+	
  @RequestMapping(value="/register",method=RequestMethod.GET)
  public String getRegistration(Model model)
  {
@@ -18,6 +24,12 @@ public class RegistrationController
 	spitter.setFirstName("Piyush");
 	spitter.setLastName("Arora");
 	model.addAttribute(spitter);
+	Order order=new Order();
+	order.setId("123");
+	order.setCustomer("Piyush Arora Hello");
+	mongo.save(order);
+	Order order1=mongo.findById("123",Order.class);
+	System.out.println("Customer retrieved from DB : "+order1.getCustomer());
 	return "register"; 
  }
  
